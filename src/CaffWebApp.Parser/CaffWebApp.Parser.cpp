77615e2280d315c1ee11469d86e5dec0a361e0a2
Gabor Bela Bognar <gaborbognar99@gmail.com>
2022-11-05 18:05:25 +0100

    Add header file

--- src/CaffWebApp.Parser/CaffWebApp.Parser.cpp
+++ src/CaffWebApp.Parser/CaffWebApp.Parser.cpp
@@ -1,14 +1,6 @@
 #include <cstdio>
 #include <cstring>
-
-void emptyCaffBuffer(unsigned char*, size_t);
-int parseCaffFile(FILE* fp);
-int writeBmpFile(FILE*, int, int);
-int writeBmpFileHeader(FILE*, int, int);
-int writeBmpFileInfoHeader(FILE*, int, int);
-int writeBmpFilePixels(FILE* fp, int, int, FILE* out);
-int littleEndianToInt(const unsigned char* buffer, int num_of_bytes);
-int bigEndianToInt(const unsigned char* buffer, int num_of_bytes);
+#include "CaffWebApp.Parser.hpp"
 
 const size_t L_BUFFER = 256;
 
---
+++ src/CaffWebApp.Parser/CaffWebApp.Parser.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstdio>
+
+void emptyCaffBuffer(unsigned char*, size_t);
+int parseCaffFile(FILE* fp);
+int writeBmpFile(FILE*, int, int);
+int writeBmpFileHeader(FILE*, int, int);
+int writeBmpFileInfoHeader(FILE*, int, int);
+int writeBmpFilePixels(FILE* fp, int, int, FILE* out);
+int littleEndianToInt(const unsigned char* buffer, int num_of_bytes);
+int bigEndianToInt(const unsigned char* buffer, int num_of_bytes);
