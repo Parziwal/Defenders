c820159e30eaecc0f932e7613b143657ab21302b
Gabor Bela Bognar <gaborbognar99@gmail.com>
2022-11-08 12:37:52 +0100

    Switch from FLIE* to <fstream>

--- src/CaffWebApp.Parser/CaffWebApp.Parser.cpp
+++ src/CaffWebApp.Parser/CaffWebApp.Parser.cpp
@@ -1,5 +1,6 @@
 #include "CaffWebApp.Parser.hpp"
 #include <iostream>
+#include <fstream>
 
 const size_t L_BUFFER = 256;
 
@@ -8,54 +9,49 @@ int main(int argc, char* argv[]) {
         std::cout << "Az argumentumok szama nem megfelelo" << std::endl;
         return 1;
     }
-    FILE* fp;
-    errno_t err = fopen_s(&fp, argv[1], "rb"); // b: Binaris modban olvas
-    if (err != 0) {
-        std::cout << "A fajl nem nyithato meg" << std::endl;
-        return 1;
-    }
-    int return_code = parseCaffFile(fp);
-    fclose(fp);
+    std::ifstream caff(argv[1], std::ifstream::in | std::ifstream::binary);
+    int return_code = parseCaffFile(caff);
+    caff.close();
     return return_code;
 }
 
-void emptyCaffBuffer(unsigned char* buffer, int length) {
+void emptyCaffBuffer(char* buffer, int length) {
     for (int i = 0; i < length; ++i)
         buffer[i] = 0;
 }
 
-int parseCaffFile(FILE* fp) {
-    auto caffBuffer = new unsigned char[L_BUFFER];
-    
+int parseCaffFile(std::ifstream& caff) {
+    auto caffBuffer = new char[L_BUFFER];
+
     emptyCaffBuffer(caffBuffer, L_BUFFER);
-    int read = fread(caffBuffer, 1, 9, fp);
-    if (read != 9) {
+
+    caff.read(caffBuffer, 9);
+
+    if (caff.gcount() != 9) {
         std::cout << "Tul rovid a fajl" << std::endl;
         return 1;
     }
 
-    if (*caffBuffer != 1) {
+    if (caffBuffer[0] != 1) {
         std::cout << "A fajl nem headerrel kezdodik." << std::endl;
         return 1;
     }
 
-    int block_size_little_endian = littleEndianToInt(caffBuffer + 1, 8);
-    int block_size_big_endian = bigEndianToInt(caffBuffer + 1, 8);
+    int block_size_little_endian = littleEndianToInt((const unsigned char*)(&caffBuffer[1]), 8);
+    int block_size_big_endian = bigEndianToInt((const unsigned char*)(&caffBuffer[1]), 8);
 
     int block_size;
     int (*endianConverter)(const unsigned char*, int);
-    if (block_size_little_endian < block_size_big_endian) {
+    if (block_size_little_endian == 20) {
         endianConverter = &littleEndianToInt;
         block_size = block_size_little_endian;
     }
-    else {
+    else if (block_size_big_endian == 20) {
         endianConverter = &bigEndianToInt;
         block_size = block_size_big_endian;
     }
-
-
-    if (block_size != 20) {
-        std::cout << "Az elso blokk mindig 20 bajtos." << std::endl;
+    else {
+        std::cout << "Rossz header" << std::endl;
         return 1;
     }
 
@@ -63,8 +59,8 @@ int parseCaffFile(FILE* fp) {
     /// 0-3 byte: 'CAFF'
     /// 4-11 byte: header_size
     /// 12-19 byte: num_anim
-    read = fread(caffBuffer, 1, 20, fp);
-    if (read != 20) {
+    caff.read(caffBuffer, 20);
+    if (caff.gcount() != 20) {
         std::cout << "Tul rovid a fajl" << std::endl;
         return 1;
     }
@@ -74,18 +70,18 @@ int parseCaffFile(FILE* fp) {
         return 1;
     }
     /// header_size
-    int header_size = (*endianConverter)(caffBuffer + 4, 8);
+    int header_size = (*endianConverter)((const unsigned char*)(&caffBuffer[4]), 8);
     if (header_size != 20) {
         std::cout << "Az header blokk mindig 20 bajtos." << std::endl;
         return 1;
     }
     /// num_anim (for annual GIF conversion in the future)
-    int num_anim = (*endianConverter)(caffBuffer + 12, 8);
+    int num_anim = (*endianConverter)((const unsigned char*)(&caffBuffer[12]), 8);
 
     /// NEW BLOCK!!!
     emptyCaffBuffer(caffBuffer, L_BUFFER);
-    read = fread(caffBuffer, 1, 9, fp);
-    if (read != 9) {
+    caff.read(caffBuffer, 9);
+    if (caff.gcount() != 9) {
         std::cout << "Tul rovid a fajl" << std::endl;
         return 1;
     }
@@ -93,7 +89,7 @@ int parseCaffFile(FILE* fp) {
         std::cout << "CAFF credits blokk kovetkezne" << std::endl;
         return 1;
     }
-    int credits_header_size = (*endianConverter)(caffBuffer + 1, 8);
+    int credits_header_size = (*endianConverter)((const unsigned char*)(&caffBuffer[1]), 8);
     if (credits_header_size < 14) {
         std::cout << "Tul kicsi a CAFF Credits blokk 'length' erteke" << std::endl;
         return 1;
@@ -107,8 +103,8 @@ int parseCaffFile(FILE* fp) {
     /// 6-13 idx byte: creator_length
     /// 14-(14+creator_length-1): creator
     emptyCaffBuffer(caffBuffer, L_BUFFER);
-    read = fread(caffBuffer, 1, credits_header_size, fp);
-    if (read != credits_header_size) {
+    caff.read(caffBuffer, credits_header_size);
+    if (caff.gcount() != credits_header_size) {
         std::cout << "Tul rovid a fajl" << std::endl;
         return 1;
     }
@@ -122,9 +118,9 @@ int parseCaffFile(FILE* fp) {
         /// NEW BLOCK
         emptyCaffBuffer(caffBuffer, L_BUFFER);
         if (i != 0)
-            fseek(fp, jump_to, SEEK_SET);
-        read = fread(caffBuffer, 1, 9, fp);
-        if (read != 9) {
+            caff.seekg(jump_to);
+        caff.read(caffBuffer, 9);
+        if (caff.gcount() != 9) {
             std::cout << "Tul rovid a fajl" << std::endl;
             return 1;
         }
@@ -134,13 +130,13 @@ int parseCaffFile(FILE* fp) {
             return 1;
         }
 
-        int anim_block_size = (*endianConverter)(caffBuffer + 1, 8);
+        int anim_block_size = (*endianConverter)((const unsigned char*)(&caffBuffer[1]), 8);
         jump_to += 9 + anim_block_size;
 
         /// CAFF Animation block
         emptyCaffBuffer(caffBuffer, L_BUFFER);
-        read = fread(caffBuffer, 1, 8, fp);
-        if (read != 8) {
+        caff.read(caffBuffer, 8);
+        if (caff.gcount() != 8) {
             std::cout << "Tul rovid a fajl" << std::endl;
             return 1;
         }
@@ -148,8 +144,8 @@ int parseCaffFile(FILE* fp) {
         //int duration = (*endianConverter)(caffBuffer, 8);
 
         emptyCaffBuffer(caffBuffer, L_BUFFER);
-        read = fread(caffBuffer, 1, 36, fp);
-        if (read != 36) {
+        caff.read(caffBuffer, 36);
+        if (caff.gcount() != 36) {
             std::cout << "Tul rovid a fajl" << std::endl;
             return 1;
         }
@@ -159,14 +155,14 @@ int parseCaffFile(FILE* fp) {
             return 1;
         }
 
-        int ciff_header_size = (*endianConverter)(caffBuffer + 4, 8);
-        int ciff_content_size = (*endianConverter)(caffBuffer + 12, 8);
+        int ciff_header_size = (*endianConverter)((const unsigned char*)(&caffBuffer[4]), 8);
+        int ciff_content_size = (*endianConverter)((const unsigned char*)(&caffBuffer[12]), 8);
         if (anim_block_size != 8 + ciff_header_size + ciff_content_size) {
             std::cout << "Nem megfelelo CIFF fajl meret es CAFF animation block meret" << std::endl;
             return 1;
         }
-        int ciff_width = (*endianConverter)(caffBuffer + 20, 8);
-        int ciff_height = (*endianConverter)(caffBuffer + 28, 8);
+        int ciff_width = (*endianConverter)((const unsigned char*)(&caffBuffer[20]), 8);
+        int ciff_height = (*endianConverter)((const unsigned char*)(&caffBuffer[28]), 8);
 
         // nehany matematikai ellenorzes a headeren
         if (ciff_content_size % 3 != 0) {
@@ -183,40 +179,34 @@ int parseCaffFile(FILE* fp) {
         }
 
         emptyCaffBuffer(caffBuffer, L_BUFFER);
-        read = fread(caffBuffer, 1, (size_t)(ciff_header_size)-36, fp);
-        if (read != ciff_header_size - 36) {
+        caff.read(caffBuffer, ciff_header_size - 36);
+        if (caff.gcount() != ciff_header_size - 36) {
             std::cout << "Tul rovid a fajl" << std::endl;
             return 1;
         }
-        if (i == 0) // csak az elso kepet mentjuk ki, viszont a többit headert is megvizsgaljuk
-            writeBmpFile(fp, ciff_width, ciff_height);
+        if (i == 0)
+            writeBmpFile(caff, ciff_width, ciff_height);
     }
-    delete[] caffBuffer;
     return 0;
 }
 
-int writeBmpFile(FILE* fp, int width, int height) {
+int writeBmpFile(std::ifstream& caff, int width, int height) {
     /// CREATE and WRITE bmp file
-    FILE* out;
-    errno_t err = fopen_s(&out, "out.bmp", "wb");
-    if (err != 0) {
-        std::cout << "Nem sikerult megnyitni a kimeneti fajlt." << std::endl;
-        return 1;
-    }
+    std::ofstream out("out.bmp", std::ofstream::out | std::ios_base::binary);
 
     writeBmpFileHeader(out, width, height);
     writeBmpFileInfoHeader(out, width, height);
-    writeBmpFilePixels(fp, width, height, out);
-    fclose(out);
+    writeBmpFilePixels(caff, width, height, out);
+    out.close();
     return 0;
 }
 
-int writeBmpFileHeader(FILE* out, int width, int height) {
+int writeBmpFileHeader(std::ofstream& out, int width, int height) {
     /// BMP HEADER
     // calculate bmp file size in bytes
     int bmp_length = width * height * 3 + 54;
 
-    unsigned char bmp_header[14];
+    char bmp_header[14];
     emptyCaffBuffer(bmp_header, 14);
     bmp_header[0] = 'B'; bmp_header[1] = 'M';
     bmp_header[2] = bmp_length | 0x00;
@@ -225,14 +215,14 @@ int writeBmpFileHeader(FILE* out, int width, int height) {
     bmp_header[5] = bmp_length >> 24 | 0x00;
     bmp_header[10] = 54;
     bmp_header[11] = bmp_header[12] = bmp_header[13] = 0;
-    fwrite(bmp_header, sizeof(bmp_header), 1, out);
+    out.write(bmp_header, sizeof(bmp_header));
     return 0;
 }
 
-int writeBmpFileInfoHeader(FILE* out, int width, int height) {
+int writeBmpFileInfoHeader(std::ofstream& out, int width, int height) {
     int content_size = width * height * 3;
 
-    unsigned char info_header[40];
+    char info_header[40];
     emptyCaffBuffer(info_header, 40);
     info_header[0] = 40;
     info_header[1] = info_header[2] = info_header[3] = 0;
@@ -256,51 +246,46 @@ int writeBmpFileInfoHeader(FILE* out, int width, int height) {
     info_header[22] = content_size >> 16 | 0x00;
     info_header[23] = content_size >> 24 | 0x00;
 
-    fwrite(info_header, sizeof(info_header), 1, out);
+    out.write(info_header, sizeof(info_header));
     return 0;
 }
 
-int writeBmpFilePixels(FILE* fp, int width, int height, FILE* out) {
+int writeBmpFilePixels(std::ifstream& caff, int width, int height, std::ofstream& out) {
     /// BMP PIXEL information
     int content_size = width * height * 3;
     int window_size = width * 3;
-    auto* bmp_pixel_buffer = new unsigned char[window_size];
-    int seek_ret = fseek(fp, content_size - window_size, SEEK_CUR);
-    if (seek_ret != 0)
-        std::cout << "fseek not worked" << std::endl;
+    auto bmp_pixel_buffer = new char[window_size];
+    caff.seekg(content_size - window_size, std::ios_base::cur);
     for (int i = 0; i < height; ++i) {
         emptyCaffBuffer(bmp_pixel_buffer, window_size);
-        int read = fread(bmp_pixel_buffer, 1, window_size, fp);
-        if (read != window_size) {
+        caff.read(bmp_pixel_buffer, window_size);
+        if (caff.gcount() != window_size) {
             std::cout << "Tul rovid a fajl" << std::endl;
             return 1;
         }
         for (int j = 0; j < window_size; j += 3) {
-            unsigned char temp = bmp_pixel_buffer[j];
+            char temp = bmp_pixel_buffer[j];
             bmp_pixel_buffer[j] = bmp_pixel_buffer[j + 2];
             bmp_pixel_buffer[j + 2] = temp;
         }
-        fwrite(bmp_pixel_buffer, sizeof(char), window_size, out);
+        out.write(bmp_pixel_buffer, window_size);
         if (i + 1 != height) {
-            seek_ret = fseek(fp, -2 * window_size, SEEK_CUR);
-            if (seek_ret != 0)
-                std::cout << "fseek not worked" << std::endl;
+            caff.seekg(-2 * window_size, std::ios_base::cur);
         }
     }
-
     delete[] bmp_pixel_buffer;
     return 0;
 }
 
 int littleEndianToInt(const unsigned char* buffer, int num_of_bytes) {
-    int ret = 0;
+    unsigned int ret = 0;
     for (int i = 0; i < num_of_bytes; ++i)
         ret |= buffer[i] << i * 8;
     return ret;
 }
 
 int bigEndianToInt(const unsigned char* buffer, int num_of_bytes) {
-    int ret = 0;
+    unsigned int ret = 0;
     for (int i = num_of_bytes - 1; i >= 0; --i) {
         ret |= buffer[i] << (num_of_bytes - i - 1) * 8;
     }
--- src/CaffWebApp.Parser/CaffWebApp.Parser.hpp
+++ src/CaffWebApp.Parser/CaffWebApp.Parser.hpp
@@ -1,11 +1,11 @@
 #pragma once
 #include <iostream>
 
-void emptyCaffBuffer(unsigned char*, int);
-int parseCaffFile(FILE* fp);
-int writeBmpFile(FILE*, int, int);
-int writeBmpFileHeader(FILE*, int, int);
-int writeBmpFileInfoHeader(FILE*, int, int);
-int writeBmpFilePixels(FILE* fp, int, int, FILE* out);
+void emptyCaffBuffer(char*, int);
+int parseCaffFile(std::ifstream&);
+int writeBmpFile(std::ifstream&, int, int);
+int writeBmpFileHeader(std::ofstream&, int, int);
+int writeBmpFileInfoHeader(std::ofstream&, int, int);
+int writeBmpFilePixels(std::ifstream&, int, int, std::ofstream&);
 int littleEndianToInt(const unsigned char* buffer, int num_of_bytes);
 int bigEndianToInt(const unsigned char* buffer, int num_of_bytes);
