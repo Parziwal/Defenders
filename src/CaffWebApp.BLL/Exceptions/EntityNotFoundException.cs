c757265d26f4a218ecd9c7f7fe558328c60b3704
Stedra Kristóf <stedra99@gmail.com>
2022-11-24 19:58:53 +0100

    Fix serializer code smell

--- src/CaffWebApp.BLL/Exceptions/EntityNotFoundException.cs
+++ src/CaffWebApp.BLL/Exceptions/EntityNotFoundException.cs
@@ -1,8 +1,15 @@
-﻿namespace CaffWebApp.BLL.Exceptions;
+﻿using System.Runtime.Serialization;
 
+namespace CaffWebApp.BLL.Exceptions;
+
+[Serializable]
 public class EntityNotFoundException : Exception
 {
     public EntityNotFoundException(string? message) : base(message)
+    {
+    }
+
+    protected EntityNotFoundException(SerializationInfo info, StreamingContext context)
     {
     }
 }
