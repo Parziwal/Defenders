b8546097a68c660dd3ba935f14f08dce42194ed8
Stedra Kristóf <stedra99@gmail.com>
2022-11-24 19:02:36 +0100

    Source out authentication

---
+++ src/CaffWebApp.Api/Authentication/AuthenticationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using CaffWebApp.Api.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace CaffWebApp.Api.Authentication;
+
+public static class AuthenticationExtensions
+{
+    public static IServiceCollection AddCaffWebAppAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var caffApiOptions = configuration.GetSection(nameof(CaffWebApiOptions)).Get<CaffWebApiOptions>()!;
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Audience = caffApiOptions.Audience;
+                options.Authority = caffApiOptions.Authority;
+            });
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("ApiScope", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", "api1");
+            });
+        });
+
+        return services;
+    }
+}
