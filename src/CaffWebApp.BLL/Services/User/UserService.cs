a4b46dfcd289d531565b63d6b738361309f36d33
Stedra Kristóf <stedra99@gmail.com>
2022-11-24 20:07:21 +0100

    Check current user

--- src/CaffWebApp.BLL/Services/User/UserService.cs
+++ src/CaffWebApp.BLL/Services/User/UserService.cs
@@ -1,7 +1,9 @@
 ﻿using CaffWebApp.BLL.Dtos.User;
 using CaffWebApp.BLL.Exceptions;
+using CaffWebApp.BLL.Extensions;
 using CaffWebApp.DAL;
 using CaffWebApp.DAL.Entites;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,17 +13,22 @@ public class UserService : IUserService
 {
     private readonly CaffDbContext _dbContext;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IHttpContextAccessor _httpContext;
 
-    public UserService(CaffDbContext dbContext, UserManager<ApplicationUser> userManager)
+    public UserService(
+        CaffDbContext dbContext,
+        UserManager<ApplicationUser> userManager,
+        IHttpContextAccessor httpContext)
     {
         _dbContext = dbContext;
         _userManager = userManager;
+        _httpContext = httpContext;
     }
 
     public async Task DeleteUser(string userId)
     {
         var user = await _dbContext.Users
-                    .SingleOrDefaultAsync(user => user.Id == userId);
+                    .SingleOrDefaultAsync(user => user.Id == userId && user.Id != _httpContext.GetCurrentUserId());
 
         if (user == null)
         {
