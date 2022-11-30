79598ca69541ff38ed55a7d9c5f3a0d264cf6f3c
Bálint Czémán <czemanb@gmail.com>
2022-11-30 22:53:53 +0100

    Add details page design

--- src/CaffWebApp.Web/src/app/pages/details-page/details-page.component.ts
+++ src/CaffWebApp.Web/src/app/pages/details-page/details-page.component.ts
@@ -9,6 +9,38 @@ export class DetailsPageComponent implements OnInit {
 
   constructor() { }
 
+  caff =  {
+    "id": 0,
+    "creatorName": "string",
+    "animationDuration": 0,
+    "fileName": "string",
+    "createdAt": "2022-11-30T20:27:04.040Z",
+    "uploadedBy": {
+      "id": "string",
+      "fullName": "string",
+      "email": "string"
+    },
+    "uploadedAt": "2022-11-30T20:27:04.040Z",
+    "ciffImages": [
+      {
+        "caption": "string",
+        "width": 0,
+        "height": 0,
+        "tags": [
+          "string"
+        ]
+      }
+    ],
+    "comments": [
+      {
+        "id": 0,
+        "text": "string",
+        "createdBy": "string",
+        "createAt": "2022-11-30T20:27:04.040Z"
+      }
+    ]
+  }
+
   ngOnInit(): void {
   }
 
